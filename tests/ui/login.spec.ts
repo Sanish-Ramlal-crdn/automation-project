899812e6a5d457b9a04adad0f139b30f42356d7b
Sanish-Ramlal-crdn <Sanish.Ramlal@dayforce.com>
2025-06-18 14:56:07 +0400

    adding invalid account login

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -11,13 +11,27 @@ test('valid login', async ({ page }) => {  //Test for a valid login
     await page.goto('https://practicesoftwaretesting.com/accounts');
 });
 
-test.only('invalid login', async ({ page }) => {  //Test for an invalid login
+test('invalid password', async ({ page }) => {  //Test for an invalid login password
     await page.goto('https://practicesoftwaretesting.com/');
     await page.locator('[data-test="nav-sign-in"]').click();
 
     await page.locator('[data-test="email"]').fill('John.Doe@gmail.com');
+    await page.locator('[data-test="password"]').fill('JohnDoe1+-');
+    await page.locator('[data-test="login-submit"]').click();
+
+    await expect(page.locator('[data-test="login-error"]')).toBeVisible;
+    
+    await page.pause()
+});
+
+test('invalid account', async ({ page }) => {  //Test for an invalid account
+    await page.goto('https://practicesoftwaretesting.com/');
+    await page.locator('[data-test="nav-sign-in"]').click();
+
+    await page.locator('[data-test="email"]').fill('pohn.Doe@gmail.com');
     await page.locator('[data-test="password"]').fill('JohnDoe1+');
     await page.locator('[data-test="login-submit"]').click();
 
-    await expect(page).not.toHaveTitle("Overview - Practice Software Testing - Toolshop - v5.0");
+    await expect(page.locator('[data-test="login-error"]')).toBeVisible;
+
 });
