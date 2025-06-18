aedd26f6779317c33abf688c3a0123a3fc9c19a3
Sanish-Ramlal-crdn <Sanish.Ramlal@dayforce.com>
2025-06-18 15:33:51 +0400

    Using POM for the login tests

---
+++ tests/pages/LoginPage.ts
@@ -0,0 +1,26 @@
+//Page Object model demo class
+export  class LoginPage {
+
+    private page;
+    private email_textbox;
+    private password_textbox;
+    private login_button;
+
+    constructor(page) {
+        this.page = page;
+        this.email_textbox = page.locator('#email');
+        this.password_textbox = page.locator('#password');
+        this.login_button = page.locator('input[value="Login"]');
+    }
+
+
+    async GoToLoginPage(){
+        await this.page.goto('https://practicesoftwaretesting.com/auth/login');
+    }
+
+    async Login(email: string, password: string){
+        await this.email_textbox.fill(email)
+        await this.password_textbox.fill(password)
+        await this.login_button.click()
+    }
+}
--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,37 +1,36 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage.ts'
+
 
 test('valid login', async ({ page }) => {  //Test for a valid login
     await page.goto('https://practicesoftwaretesting.com/');
-    await page.locator('[data-test="nav-sign-in"]').click();
+    const login = new LoginPage(page)
+    await login.GoToLoginPage()
 
-    await page.locator('[data-test="email"]').fill('John.Doe@gmail.com');
-    await page.locator('[data-test="password"]').fill('JohnDoe1+');
-    await page.locator('[data-test="login-submit"]').click();
+    await login.Login('John.Doe@gmail.com','JohnDoe1+')
 
     await page.goto('https://practicesoftwaretesting.com/accounts');
 });
 
 test('invalid password', async ({ page }) => {  //Test for an invalid login password
     await page.goto('https://practicesoftwaretesting.com/');
-    await page.locator('[data-test="nav-sign-in"]').click();
+    const login = new LoginPage(page)
+    await login.GoToLoginPage()
 
-    await page.locator('[data-test="email"]').fill('John.Doe@gmail.com');
-    await page.locator('[data-test="password"]').fill('JohnDoe1+-');
-    await page.locator('[data-test="login-submit"]').click();
+    await login.Login('John.Doe@gmail.com','JohnDoe1+-')
 
     await expect(page.locator('[data-test="login-error"]')).toBeVisible;
-    
-    await page.pause()
+
+
 });
 
 test('invalid account', async ({ page }) => {  //Test for an invalid account
     await page.goto('https://practicesoftwaretesting.com/');
-    await page.locator('[data-test="nav-sign-in"]').click();
+    const login = new LoginPage(page)
+    await login.GoToLoginPage()
 
-    await page.locator('[data-test="email"]').fill('pohn.Doe@gmail.com');
-    await page.locator('[data-test="password"]').fill('JohnDoe1+');
-    await page.locator('[data-test="login-submit"]').click();
+    await login.Login('pohn.Doe@gmail.com','JohnDoe1+')
 
-    await expect(page.locator('[data-test="login-error"]')).toBeVisible;
+    await expect(page.locator('[data-test="login-error"]')).toBeVisible
 
 });
