facbec3020c01170a4c4de224d8784d4ec81586e
Sanish-Ramlal-crdn <Sanish.Ramlal@dayforce.com>
2025-06-19 10:57:45 +0400

    Adding invalid checkout test

--- tests/ui/checkout.spec.ts
+++ tests/ui/checkout.spec.ts
@@ -5,7 +5,7 @@ import { LoginPage } from '../pages/LoginPage.ts'
 import { CheckoutPage } from '../pages/CheckoutPage.ts'
 
 //Testing for how 1 product is added to the cart
-test('checkout', async ({ page }) => {  //Test for clicking an item and adding it to the cart
+test('valid checkout', async ({ page }) => {  
     await page.goto('https://practicesoftwaretesting.com/');
 
     await page.locator('h5.card-title:has-text("Combination Pliers")').click();
@@ -36,4 +36,34 @@ test('checkout', async ({ page }) => {  //Test for clicking an item and adding i
     // Getting the inovice number
     const invoiceNumber = await page.$eval('div#order-confirmation span', element => element.textContent);
     console.log(`Order ${invoiceNumber} placed and verified successfully!`);
+});
+
+//Testing invalid checkout with invalid payment
+test.only('invalid checkout', async ({ page }) => {  
+    await page.goto('https://practicesoftwaretesting.com/');
+
+    await page.locator('h5.card-title:has-text("Combination Pliers")').click();
+
+    const product = new ProductPage(page);
+    await product.AddToCart();
+    await page.getByRole('alert', { name: 'Product added to shopping' }).click();
+    await page.locator('[data-test="nav-cart"]').click();
+
+    //Checking if the proper item has been added to the cart;
+    await expect(page.locator('[data-test="product-title"]')).toHaveText('Combination Pliers');
+
+    //Login
+    const cart = new CartPage(page);
+    await cart.GoToLogin();
+    const login = new LoginPage(page)
+    await login.Login('John.Doe@gmail.com', 'JohnDoe1+')
+
+    await cart.GoToBilling();
+    await cart.GoToCheckout();
+    await page.locator('[data-test="payment-method"]').selectOption('bank-transfer');
+    const checkout = new CheckoutPage(page);
+    await checkout.CompleteOrder("Test Bank", "Test Account", "1234567");
+
+    expect(page.locator('[class="alert alert-danger ng-star-inserted]')).toBeVisible;
+    console.log("Invalid bank account number!")
 });
