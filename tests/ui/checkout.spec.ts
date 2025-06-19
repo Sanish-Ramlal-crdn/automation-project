8ea84dbfbea3df680b98c72b2019c3313922549b
Sanish-Ramlal-crdn <Sanish.Ramlal@dayforce.com>
2025-06-19 11:44:55 +0400

    Adjusting checkout logic

--- tests/ui/checkout.spec.ts
+++ tests/ui/checkout.spec.ts
@@ -5,7 +5,7 @@ import { LoginPage } from '../pages/LoginPage.ts'
 import { CheckoutPage } from '../pages/CheckoutPage.ts'
 
 //Testing for how 1 product is added to the cart
-test('valid checkout', async ({ page }) => {  
+test.only('valid checkout', async ({ page }) => {
     await page.goto('https://practicesoftwaretesting.com/');
 
     await page.locator('h5.card-title:has-text("Combination Pliers")').click();
@@ -22,7 +22,32 @@ test('valid checkout', async ({ page }) => {
     const cart = new CartPage(page);
     await cart.GoToLogin();
     const login = new LoginPage(page)
-    await login.Login('John.Doe@gmail.com', 'JohnDoe1+')
+    await page.pause();
+    await login.Login('John.Doe@gmail.com', 'JohnDoe1+');
+
+
+    const isLoginErrorVisible = await page.locator('[data-test="login-error"]').isVisible();
+
+    if (isLoginErrorVisible) {
+        await page.locator('[data-test="register-link"]').click();
+        await page.locator('[data-test="first-name"]').fill('John');
+        await page.locator('[data-test="last-name"]').fill('Doe');
+        await page.locator('[data-test="dob"]').fill('2001-01-01');
+        await page.locator('[data-test="street"]').fill('Abbey Road');
+        await page.locator('[data-test="postal_code"]').fill('12345');
+        await page.locator('[data-test="city"]').fill('London');
+        await page.locator('[data-test="state"]').fill('London');
+        await page.locator('[data-test="country"]').selectOption('GB');
+        await page.locator('[data-test="phone"]').fill('1234567');
+        await page.locator('[data-test="email"]').fill('John.Doe@gmail.com');
+        await page.locator('[data-test="password"]').fill('JohnDoe1+');
+        await page.locator('[data-test="register-submit"]').click();
+        const login = new LoginPage(page)
+        await login.Login('John.Doe@gmail.com', 'JohnDoe1+');
+        await page.locator('[data-test="nav-cart"]').click();
+        const cart = new CartPage(page);
+        await cart.GoToLogin();
+    } 
 
     await cart.GoToBilling();
     await cart.GoToCheckout();
@@ -36,10 +61,12 @@ test('valid checkout', async ({ page }) => {
     // Getting the inovice number
     const invoiceNumber = await page.$eval('div#order-confirmation span', element => element.textContent);
     console.log(`Order ${invoiceNumber} placed and verified successfully!`);
+
+
 });
 
 //Testing invalid checkout with invalid payment
-test('invalid checkout', async ({ page }) => {  
+test('invalid checkout', async ({ page }) => {
     await page.goto('https://practicesoftwaretesting.com/');
 
     await page.locator('h5.card-title:has-text("Combination Pliers")').click();
@@ -57,6 +84,29 @@ test('invalid checkout', async ({ page }) => {
     await cart.GoToLogin();
     const login = new LoginPage(page)
     await login.Login('John.Doe@gmail.com', 'JohnDoe1+')
+
+        const isLoginErrorVisible = await page.locator('[data-test="login-error"]').isVisible();
+
+    if (isLoginErrorVisible) {
+        await page.locator('[data-test="register-link"]').click();
+        await page.locator('[data-test="first-name"]').fill('John');
+        await page.locator('[data-test="last-name"]').fill('Doe');
+        await page.locator('[data-test="dob"]').fill('2001-01-01');
+        await page.locator('[data-test="street"]').fill('Abbey Road');
+        await page.locator('[data-test="postal_code"]').fill('12345');
+        await page.locator('[data-test="city"]').fill('London');
+        await page.locator('[data-test="state"]').fill('London');
+        await page.locator('[data-test="country"]').selectOption('GB');
+        await page.locator('[data-test="phone"]').fill('1234567');
+        await page.locator('[data-test="email"]').fill('John.Doe@gmail.com');
+        await page.locator('[data-test="password"]').fill('JohnDoe1+');
+        await page.locator('[data-test="register-submit"]').click();
+        const login = new LoginPage(page)
+        await login.Login('John.Doe@gmail.com', 'JohnDoe1+');
+        await page.locator('[data-test="nav-cart"]').click();
+        const cart = new CartPage(page);
+        await cart.GoToLogin();
+    } 
 
     await cart.GoToBilling();
     await cart.GoToCheckout();
